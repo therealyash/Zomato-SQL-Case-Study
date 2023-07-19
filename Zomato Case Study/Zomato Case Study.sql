af48167d866f2d807bff1f8e6c2bc33c4dcb43ad
therealyash <yashraj1416@gmail.com>
2023-07-20 03:13:43 +0530

    4,5 Case Study Questions Solved

--- Zomato Case Study/Zomato Case Study.sql
+++ Zomato Case Study/Zomato Case Study.sql
@@ -16,8 +16,41 @@ GROUP BY 1
 ORDER BY 2 DESC
 
 3. Find top restaurants in terms of orders for a given month
+
+SELECT r_name, COUNT(order_id) AS orders
+FROM restaurants r
+JOIN orders o 
+ON r.r_id = o.r_id
+WHERE MONTHNAME(date) = 'June'
+GROUP BY r_name
+ORDER BY 2 DESC
+LIMIT 1
+
 4. Restaurants with monthly sales > x for
+
+SELECT r_name, SUM(amount) AS revenue
+FROM orders o
+JOIN restaurants r
+ON o.r_id = r.r_id
+WHERE MONTHNAME(date) LIKE 'July' 
+GROUP BY 1
+HAVING SUM(amount) > 499
+ORDER BY 2 DESC
+
 5. Show all with order details for a particular customer in a particular date range
+
+SELECT o.order_id, r.r_name, f.f_name
+FROM orders o
+JOIN restaurants r
+ON o.r_id = r.r_id
+JOIN order_details od
+ON od.order_id = o.order_id
+JOIN food f
+ON f.f_id = od.f_id
+WHERE user_id = (SELECT user_id FROM users
+                 WHERE name LIKE 'Ankit')
+AND date > '2022-06-10' AND date < '2022-07-10'
+
 6. Find restaurants with max repeated customers
 7. Month over month revenue of Zomato
 8. Customer -> favourite food
