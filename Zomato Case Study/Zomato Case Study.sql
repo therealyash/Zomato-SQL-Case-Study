bb3c7e36564cca4b3ac958d5c239b25920cb0826
therealyash <yashraj1416@gmail.com>
2023-07-19 20:55:44 +0530

    Initial Commit

---
+++ Zomato Case Study/Zomato Case Study.sql
@@ -0,0 +1,19 @@
+Zomato Case Study
+
+1. Find customers who have never ordered. 
+2. Average Price/Dish
+3. Find top restaurants in terms of orders for a given month
+4. Restaurants with monthly sales > x for
+5. Show all with order details for a particular customer in a particular date range
+6. Find restaurants with max repeated customers
+7. Month over month revenue of Zomato
+8. Customer -> favourite food
+
+Extra Questions
+
+1. Find most loyal customers for all restaurants.
+2. Month over month revenue growth of a restaurant
+3. Most paired products
+
+
+
