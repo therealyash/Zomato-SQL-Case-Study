d642a209e861d2dbe9764c96ba2d756ff9cf980c
therealyash <yashraj1416@gmail.com>
2023-07-19 21:04:17 +0530

    Average Price Solution

--- Zomato Case Study/Zomato Case Study.sql
+++ Zomato Case Study/Zomato Case Study.sql
@@ -1,7 +1,20 @@
 Zomato Case Study
 
 1. Find customers who have never ordered. 
+
+SELECT name 
+FROM users
+WHERE user_id NOT IN (SELECT user_id FROM orders)
+
 2. Average Price/Dish
+
+SELECT f_name, ROUND(AVG(price),2) AS 'avg_price'
+FROM menu m
+JOIN food f
+ON m.f_id = f.f_id
+GROUP BY 1
+ORDER BY 2 DESC
+
 3. Find top restaurants in terms of orders for a given month
 4. Restaurants with monthly sales > x for
 5. Show all with order details for a particular customer in a particular date range
